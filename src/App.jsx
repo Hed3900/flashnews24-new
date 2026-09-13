270bff54938e74f0da36998e6c2c42670e09e38e
Hed3900 <soundaryat404@gmail.com>
2026-09-13 18:11:55 +0530

    Configure new.flashnews24.site

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import InfoPage from "./pages/InfoPage";
 
 function App() {
   return (
-    <BrowserRouter basename="/flashnews24-new">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/article/:slug" element={<Article />} />
